e80ea9905340b2a3f6283d9aba2ebdfb25733d29
cryptodecker <cryptodecker@15c187da-9f1a-11de-b98f-4500b42dc123>
2009-10-02 06:22:32 +0000

    v1.1 - Fixed applet parameters.

---
+++ tags/AboutMe.v1.1/AboutMe/src/AboutMe.java
@@ -0,0 +1,43 @@
+/**
+ * @(#)AboutMe.java
+ *
+ * AboutMe Applet application
+ *
+ * @author J.A. Simmons V
+ * @version 1.00 2009/9/12
+ */
+
+import java.awt.*;
+import java.applet.*;
+
+public class AboutMe extends Applet 
+{
+	private static final long serialVersionUID = 1L;
+
+	public void paint(Graphics g) {
+		
+		Image img = getImage( getCodeBase(), "JABluebonnets.JPG");
+		Font small = new Font("Serif", Font.PLAIN, 8);
+		Font medium = new Font("Serif", Font.ITALIC, 12);
+		Font large = new Font("Serif", Font.PLAIN, 25);
+		
+		g.setColor(Color.blue);
+		g.drawString("James A Simmons V", 220, 350 );
+		
+		g.setFont(large);
+		g.setColor(Color.red);
+		g.drawString("Simmons", 220, 380 );
+		
+		g.setFont(medium);
+		g.setColor(Color.CYAN);
+		g.drawString("jsimmons", 220, 400 );
+		
+		g.setColor(Color.black);
+		g.setFont(small);
+		g.drawString("Circumnavigated the Globe in the Navy", 220, 410 );
+		
+		g.drawImage(img, 0, 0, 550,309,this);
+		
+	}//end method paint
+}//end class AboutMe
+
