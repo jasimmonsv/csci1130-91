7e31fdda8bc36da9af7bcfe6453a77a171dec8c1
cryptodecker <cryptodecker@15c187da-9f1a-11de-b98f-4500b42dc123>
2009-10-28 05:32:32 +0000

    v1.0 - added JTextField for user input on num of stars to draw.

--- MilkyWay/src/MilkyWay.java
+++ MilkyWay/src/MilkyWay.java
@@ -8,21 +8,44 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MilkyWay extends JApplet
+public class MilkyWay extends JApplet implements ActionListener
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6575932833188924720L;
 	int MAX_DIMTR=100;
-	int MAX_PLANETS=100;
+	int maxPlanets=100;
 	private final Random randomNumber = new Random((int) (Math.random()*10+1));
 	private final int SCREEN_WIDTH=700;
 	private final int SCREEN_HEIGTH=500;
+	private JButton btnSubmit;
+	private JTextField numStars;
+	private Graphics g;
+	
+	public void init()
+	{
+		setLayout( new BorderLayout() );
+		numStars = new JTextField(10);
+		btnSubmit = new JButton("Submit");
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new FlowLayout());
+		topPanel.add(numStars);
+		topPanel.add(btnSubmit);
+		btnSubmit.addActionListener(this);
+		add(topPanel, BorderLayout.NORTH);		
+	}//end method init */
 	
 	public void paint(Graphics g)
 	{
-		g.fillRect(0, 0, 800, 600);
+		super.paint(g);
+		g.fillRect(0, 40, 800, 600);
 		g.setColor(Color.white);
-		for (int i=1; i<=MAX_PLANETS*3; i++) g.fillOval(randomNumber.nextInt(SCREEN_WIDTH+100), randomNumber.nextInt(SCREEN_HEIGTH+100), 2,2 );
-		for (int i=1;i<=MAX_PLANETS;i++)
+		for (int i=1; i<=300; i++) g.fillOval(randomNumber.nextInt(SCREEN_WIDTH+100), randomNumber.nextInt(SCREEN_HEIGTH+100), 2,2 ); //draws Background of stars
+		for (int i=1;i<=maxPlanets;i++)
 		{
 			paintPlanet( g );
 		}
@@ -32,6 +55,16 @@ public class MilkyWay extends JApplet
 	{
 		int dimtr = randomNumber.nextInt( MAX_DIMTR );	
 		g.setColor(new Color( randomNumber.nextInt(255), randomNumber.nextInt(255), randomNumber.nextInt(255) ));//set a random color and alpha for the planets
-		g.fillOval(randomNumber.nextInt(SCREEN_WIDTH), randomNumber.nextInt(SCREEN_HEIGTH), dimtr,dimtr ); //draw the planet of random width and heigth
+		g.fillOval(randomNumber.nextInt(SCREEN_WIDTH), randomNumber.nextInt(SCREEN_HEIGTH)+40, dimtr,dimtr ); //draw the planet of random width and heigth
 	}//end paintPlanet Method */
+	public void actionPerformed( ActionEvent ae )
+	{
+		Object obj = ae.getSource();
+		if (obj == btnSubmit)
+		{
+			maxPlanets=Integer.parseInt(numStars.getText());
+			super.repaint();
+		}//end btnSubmit if */
+		
+	}//end method actionPerformed */
 }//end MilkyWay Class */
