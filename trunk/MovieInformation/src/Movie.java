91abf9a4ce844fa566a2f33a803fd8b20eb64f7a
cryptodecker <cryptodecker@15c187da-9f1a-11de-b98f-4500b42dc123>
2009-11-22 01:31:58 +0000

    #importing MovieInformation project

---
+++ trunk/MovieInformation/src/Movie.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+public class Movie extends JComponent 
+{
+	String title, director;
+	String date;
+	Image img;
+	
+	//--constructor
+	public Movie () 
+	{
+		
+	}//end constructor */
+	
+	public void getDirector()
+	{
+		
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		
+	}//end method paintComponent */
+
+}//end class Movie */
